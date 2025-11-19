970b0b07f621e83ec7468e5e01e9bbb90260209c
k1rson <vlad2k15@bk.ru>
2025-11-19 08:45:28 +0300

    update main.py

---
+++ rpg-x/entities/enemy/__init__.py
@@ -0,0 +1,15 @@
+from .bear import BearEntity
+from .dog import DogEntity
+from .goblin import GoblinEntity
+from .rafik import RafikEntity
+from .skeleton import SkeletonEntity
+from .spider import SpiderEntity
+
+__all__ = [
+    "BearEntity",
+    "DogEntity",
+    "GoblinEntity",
+    "RafikEntity",
+    "SkeletonEntity",
+    "SpiderEntity",
+]
---
+++ rpg-x/main.py
@@ -0,0 +1,13 @@
+import os
+import random
+
+# Импорты сущностей
+from entities.player.player import PlayerEntity
+from entities.enemy import (
+    BearEntity,
+    DogEntity,
+    GoblinEntity,
+    RafikEntity,
+    SkeletonEntity,
+    SpiderEntity,
+)
